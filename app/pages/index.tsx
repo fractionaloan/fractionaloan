02f11d15f4e61f3e7de65323508dd58ed314f7bc
Ben Yang <benjyang96@gmail.com>
2021-10-23 19:02:42 -0400

    wip

--- app/pages/index.tsx
+++ app/pages/index.tsx
@@ -3,12 +3,14 @@ import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import Footer from '../components/Footer'
 import Head from '../components/Head'
+import Header from '../components/Header';
 
 const Home: NextPage = () => {
   return (
     <div className={styles.container}>
       <Head/>
       <main className={styles.main}>
+        <Header/>
         <h1 className={styles.title}>
           Welcome to Fractionaloan
         </h1>
